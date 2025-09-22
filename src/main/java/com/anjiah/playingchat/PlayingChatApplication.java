212ae617673e02d6b25c39602b2ac18e753d2702
BigEven <2797267156@qq.com>
2025-09-22 14:22:41 +0800

    空模板

---
+++ src/main/java/com/anjiah/playingchat/PlayingChatApplication.java
@@ -0,0 +1,13 @@
+package com.anjiah.playingchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayingChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anjiah/playingchat/PlayingChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjiah.playingchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
